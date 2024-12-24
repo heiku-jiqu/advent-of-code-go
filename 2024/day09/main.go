376282a5e1aac6fd129ed770275d33f8876012c7
Heiser <heiser9295@hotmail.com>
2024-12-24 12:38:09 +0800

    day 9 part 1

--- 2024/day09/main.go
+++ 2024/day09/main.go
@@ -4,10 +4,9 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
 
-var INPUTFILE = "./2024/day09/input_test.txt"
+var INPUTFILE = "./2024/day09/input.txt"
 
 func main() {
 	fileBytes, err := os.ReadFile(INPUTFILE)
@@ -16,15 +15,19 @@ func main() {
 	}
 
 	inputString := string(fileBytes)
-	log.Print(inputString)
-	expandedFileFormat := expandFileFormat(inputString)
-	log.Print(expandedFileFormat)
+	// log.Print(inputString)
+	expandedFileFormat := expandFileFormatInt(inputString)
+	// log.Print(expandedFileFormat)
+	compacted := compact(expandedFileFormat)
+	// log.Print(compacted)
+	part1ans := checksum(compacted)
+	log.Print("Day 9 Part 1 answer is:", part1ans)
 }
 
 // expands dense file format into actual blocks
 // with ID number and . for empty
-func expandFileFormat(denseString string) string {
-	out := ""
+func expandFileFormatInt(denseString string) []int {
+	out := []int{}
 	for i, char := range denseString {
 		if i%2 == 0 {
 			// char represents size of file
@@ -33,17 +36,70 @@ func expandFileFormat(denseString string) string {
 			if err != nil {
 				log.Panic(err)
 			}
-			fileIdStr := strconv.Itoa(fileId)
-			fullFileBlock := strings.Repeat(fileIdStr, size)
-			out = out + fullFileBlock
+			fullFileBlock := make([]int, size)
+			for i := range size {
+				fullFileBlock[i] = fileId
+			}
+			out = append(out, fullFileBlock...)
 		} else {
 			// char represents size of empty space
 			size, err := strconv.Atoi(string(char))
 			if err != nil {
 				log.Panic(err)
 			}
-			fullEmptyBlock := strings.Repeat(".", size)
-			out = out + fullEmptyBlock
+			fullEmptyBlock := make([]int, size)
+			for i := range size {
+				fullEmptyBlock[i] = -1
+			}
+			out = append(out, fullEmptyBlock...)
+		}
+	}
+	return out
+}
+
+func compact(expandedFS []int) []int {
+	expanded := expandedFS
+	left := 0
+	right := len(expanded) - 1
+
+	for left < right {
+		if expanded[left] == -1 && expanded[right] != -1 {
+			expanded[left], expanded[right] = expanded[right], expanded[left]
+			// log.Print("after swap: ", string(expanded))
+			left++
+			right--
+		}
+		if expanded[left] != -1 {
+			left += 1
+		}
+		if expanded[right] == -1 {
+			right -= 1
+		}
+	}
+	return expanded
+}
+
+func toInt(s string) []int {
+	out := make([]int, len(s))
+	for i, c := range s {
+		if c == '.' {
+			out[i] = -1
+			continue
+		}
+		num, err := strconv.Atoi(string(c))
+		if err != nil {
+			log.Panic(err)
+		}
+		out[i] = num
+	}
+	return out
+}
+
+func checksum(compacted []int) int {
+	out := 0
+	for i, val := range compacted {
+		if val != -1 {
+			out += i * val
 		}
 	}
 	return out
